cef6901abcc744a78da4cf6ec328e2779753c36b
Ryan <ryannarelli12@gmail.com>
2025-02-02 21:29:28 -0300

    enviando alterações

--- src/app/components/HeroPrinciple/index.tsx
+++ src/app/components/HeroPrinciple/index.tsx
@@ -1,22 +1,21 @@
-// import { useEffect } from "react";
-// import Typed from "typed.js";
+"use client";
+import { useEffect } from "react";
+import Typed from "typed.js";
 
 export const HeroPrinciple = () => {
-  // useEffect(() => {
-  //     const typed = new Typed(".typing", {
-  //         strings: [
-  //             "FullStack Developer",
-  //             "Frontend Developer",
-  //             "Backend Developer",
-  //             "Mobile Apps Developer"
-  //         ],
-  //         typeSpeed: 100,
-  //         backSpeed: 60,
-  //         loop: true
-  //     });
+  useEffect(() => {
+      const typed = new Typed(".typing", {
+          strings: [
+              "Desenvolvedor Full Stack",
+              "UX UI Designer"
+          ],
+          typeSpeed: 100,
+          backSpeed: 60,
+          loop: true
+      });
 
-  //     return () => typed.destroy(); // Limpa a instância ao desmontar o componente
-  // }, []);
+      return () => typed.destroy(); // Limpa a instância ao desmontar o componente
+  }, []);
 
   return (
     <div className="h-full w-full mx-auto relative top-12 pb-20 xl:px-40 px-8 flex md:flex-row-reverse flex-col gap-4 justify-center items-center pt-4">
@@ -30,14 +29,6 @@ export const HeroPrinciple = () => {
         </div>
       </div>
 
-      {/* <div className="w-full relative">
-        <img
-          className="relative z-10 rounded-full max-w-[60%] mx-auto outline outline-[.7rem] outline-offset-[.1rem] outline-colorSecondary"
-          src="/img/profile.png"
-          alt="My Image"
-        />
-      </div> */}
-
       <div className="w-full flex flex-col justify-center gap-4 md:text-left text-center text-white dark:text-gray-800 md:mt-0 sm:mt-8 mt-4 font-sen">
         <h1 className="md:text-4xl sm:text-3xl text-2xl font-semibold font-serif lg:w-2/4">
           Olá, me chamo{" "}
@@ -45,7 +36,7 @@ export const HeroPrinciple = () => {
         </h1>
         <h3 className="capitalize md:text-4xl sm:text-3xl text-xl font-extrabold">
           E eu sou{" "}
-          <span className="typing text-green-500 dark:text-green-700"></span>
+          <span className="typing text-colorSecondary"></span>
         </h3>
         <p className="font-inter md:text-xl font-extrabold">
           Desenvolvedor Full Stack e UX UI Designer apaixonado por criar
