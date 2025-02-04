e3106c3d1cef9b3869f593db01f9e29568b64f77
Ryan <ryannarelli12@gmail.com>
2025-02-04 19:23:55 -0300

    enviando ajustes

--- src/app/components/Skill/index.tsx
+++ src/app/components/Skill/index.tsx
@@ -7,11 +7,11 @@ export const Skill = () => {
                 <div className="mb-7">
                     <div className="flex justify-between py-1">
                         <span className="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">React</span>
-                        <span className="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">80%</span>
+                        <span className="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">85%</span>
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#00D8FF" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '79.2px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#00D8FF" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '88px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
@@ -22,7 +22,7 @@ export const Skill = () => {
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#F0DB4F" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '79.2px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#F0DB4F" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '95px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
@@ -33,18 +33,18 @@ export const Skill = () => {
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#2D79C7" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '94.05px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#2D79C7" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '88px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
                 <div className="mb-7">
                     <div className="flex justify-between py-1">
                         <span className="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Next.js</span>
-                        <span className="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">75%</span>
+                        <span className="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">80%</span>
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#000000" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '64.35px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#000000" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '80px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
@@ -55,7 +55,7 @@ export const Skill = () => {
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#2EBEFF" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '74.25px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#2EBEFF" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '88px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
@@ -66,7 +66,7 @@ export const Skill = () => {
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#F05045" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '74.25px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#F05045" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '90px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
@@ -77,18 +77,18 @@ export const Skill = () => {
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#3498D4" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '74.25px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#3498D4" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '88px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
 
                 <div className="mb-7">
                     <div className="flex justify-between py-1">
                         <span className="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Figma</span>
-                        <span className="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">75%</span>
+                        <span className="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">80%</span>
                     </div>
                     <svg className="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
                         <path className="rc-progress-line-trail" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#D9D9D9" strokeWidth="1" fillOpacity="0"></path>
-                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#FF7262" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '74.25px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
+                        <path className="rc-progress-line-path" d="M 0.5,0.5 L 99.5,0.5" strokeLinecap="round" stroke="#FF7262" strokeWidth="1" fillOpacity="0" style={{ strokeDasharray: '80px, 100px', strokeDashoffset: '0px', transition: 'stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s' }}></path>
                     </svg>
                 </div>
             </div>
